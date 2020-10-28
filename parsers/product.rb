90fe8882ee831e7cd5b52b29802761422dfca40e
Alx_Bx <cristalexander@gmail.com>
2020-10-28 16:23:23 -0600

    add config

--- parsers/product.rb
+++ parsers/product.rb
@@ -1,16 +1,9 @@
 html = Nokogiri.HTML (content)
-products = html.css('#yt_main .yt_main_inner #catalog-listing .yt-products-container .products-grid .row .item')
+status = html.css('.availabilityi.n-stock').text.strip
 
-products.each do |product|
-    name = product.css('.item-inner .product-info .product-name > a').text.strip
-    price = product.css('.item-inner .product-info .product-price .price-box .max-prices .col-xs-12 .row .col-xs-12 .regular-price .price').text.strip
-    link = product.css(".product-info .product-name > a")[0]["href"]
-    pages << {
-        url: link,
-        page_type: 'product',
-        vars: {
-            name: name,
-            price: price
-        }
-    }
-end
+outputs << {
+    _collection: 'product',
+    status: status,
+    name: page['vars']['name'],
+    price: page['vars']['price']
+}
--- parsers/search.rb
+++ parsers/search.rb
@@ -1,9 +1,16 @@
 html = Nokogiri.HTML (content)
-status = html.css('.availabilityi.n-stock').text.strip
+products = html.css('#yt_main .yt_main_inner #catalog-listing .yt-products-container .products-grid .row .item')
 
-outputs << {
-    _collection: 'product',
-    status: status,
-    name: page['vars']['name'],
-    price: page['vars']['price']
-}
+products.each do |product|
+    name = product.css('.item-inner .product-info .product-name > a').text.strip
+    price = product.css('.item-inner .product-info .product-price .price-box .max-prices .col-xs-12 .row .col-xs-12 .regular-price .price').text.strip
+    link = product.css(".product-info .product-name > a")[0]["href"]
+    pages << {
+        url: link,
+        page_type: 'product',
+        vars: {
+            name: name,
+            price: price
+        }
+    }
+end
